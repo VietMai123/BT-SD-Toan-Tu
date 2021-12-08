2bd4fd90576bbeb246fbc79d7db76b1cbf90725c
VietMai123 <ngophucdhcn@gmail.com>
2021-12-08 16:21:47 +0700

    lan1

---
+++ BTSDbien.js
@@ -0,0 +1,8 @@
+let inputWidth;
+let inputHeiht;
+inputWidth = prompt("enter the width");
+inputHeiht = prompt("enter the heiht");
+let wight=parseInt(inputWidth);
+let height=parseInt(inputHeiht);
+let area = wight*height;
+document.write("The area is: " + area)
